4c772eecea5c6e536e77c9d79809fcfe5d20a36a
CISC181 <bgibbons@udel.edu>
2015-12-08 21:56:33 -0500

    Final Exam Template

---
+++ FirstBankMortgage_DAL/src/main/java/base/RateDAL.java
@@ -0,0 +1,28 @@
+package base;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import domain.RateDomainModel;
+import util.HibernateUtil;
+
+public class RateDAL {
+
+
+	public static double getRate(int GivenCreditScore) {
+		//FinalExam - please implement		
+		// Figure out which row makes sense- return back the 
+		// right interest rate from the table based on the given credit score
+		
+		//FinalExam - obviously change the return value
+		return 0;
+	}
+
+}
